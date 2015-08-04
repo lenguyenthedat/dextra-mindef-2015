d16dbf145fa9411f1667fa864a263b4fda33e709
Dat Le <thedat.lenguyen@gmail.com>
2015-08-04 15:55:03 +0800

    Added draft README, updated code to try more classifiers.

---
+++ README.md
@@ -0,0 +1,9 @@
+XGBClassifier(n_estimators=128,subsample=0.6,max_depth=16,min_child_weight=3)  0.0202
+XGBClassifier(n_estimators=128,subsample=1,max_depth=16,min_child_weight=3) 0.0191784
+XGBClassifier(n_estimators=128,subsample=1,max_depth=16,min_child_weight=3) No scaling 0.0202145
+XGBClassifier(n_estimators=128,subsample=0.4,max_depth=16,min_child_weight=3) No scaling 0.0198647
+XGBClassifier(n_estimators=128,subsample=1,max_depth=16,min_child_weight=3) No scaling No "RANK GROUPING" 0.0194125
+XGBClassifier(n_estimators=128,subsample=1,max_depth=16,min_child_weight=4) No scaling 0.0201803
+XGBClassifier(n_estimators=128,subsample=0.4,max_depth=16,min_child_weight=4) 0.0204031
+BEST = XGBClassifier(n_estimators=128,subsample=1,max_depth=16,min_child_weight=3) No "RANK GROUPING" 0.0191475
+XGBClassifier(n_estimators=512,subsample=0.8,max_depth=8,min_child_weight=5) 0.0201772
--- classify.py
+++ classify.py
@@ -5,14 +5,18 @@ import numpy as np
 import os
 import itertools
 
-from sklearn.metrics import log_loss
+from sklearn.metrics import log_loss, accuracy_score
+from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import AdaBoostClassifier, GradientBoostingClassifier, RandomForestClassifier
 from xgboost import XGBClassifier
-from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sknn.mlp import Classifier, Layer
 from sklearn.grid_search import GridSearchCV
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.lda import LDA
+from sklearn.qda import QDA
 
 pd.options.mode.chained_assignment = None
 
@@ -20,7 +24,7 @@ sample = False
 gridsearch = False
 
 features = ['GENDER','COUNTRY_OF_BIRTH','NATIONALITY',
-            'AGE','MARITAL_STATUS','RANK_GRADE','RANK_GROUPING',
+            'AGE','MARITAL_STATUS','RANK_GRADE',
             'YEARS_IN_GRADE','EMPLOYEE_GROUP','PARENT_SERVICE','SERVICE_SUB_AREA','SERVICE_TYPE','YEARS_OF_SERVICE',
             'VOC','UNIT','NO_OF_KIDS','MIN_CHILD_AGE','AVE_CHILD_AGE','HSP_ESTABLISHMENT','HSP_CERTIFICATE','HSP_CERT_RANK',
             'HSP_CERT_DESC','UPGRADED_LAST_3_YRS','UPGRADED_CERT_3_YRS','UPGRADED_CERT_DESC_3_YRS','MARRIED_WITHIN_2_YEARS',
@@ -29,7 +33,7 @@ features = ['GENDER','COUNTRY_OF_BIRTH','NATIONALITY',
             'HOUSING_TYPE','HOUSING_GROUP','HOUSING_RANK','PREV_HOUSING_TYPE','MOVE_HOUSE_T_2','HOUSE_UPG_DGRD','IPPT_SCORE',
             'PES_SCORE','HOMETOWORKDIST','SVC_INJURY_TYPE','TOT_PERC_INC_LAST_1_YR','BAS_PERC_INC_LAST_1_YR']
 features_non_numeric = ['GENDER','COUNTRY_OF_BIRTH','NATIONALITY',
-            'MARITAL_STATUS','RANK_GRADE','RANK_GROUPING',
+            'MARITAL_STATUS','RANK_GRADE',
             'EMPLOYEE_GROUP','PARENT_SERVICE','SERVICE_SUB_AREA','SERVICE_TYPE',
             'VOC','UNIT','HSP_ESTABLISHMENT','HSP_CERTIFICATE',
             'HSP_CERT_DESC','UPGRADED_LAST_3_YRS','UPGRADED_CERT_3_YRS','UPGRADED_CERT_DESC_3_YRS','MARRIED_WITHIN_2_YEARS',
@@ -42,9 +46,9 @@ myid = 'PERID'
 # Load data
 if sample:
     df = pd.read_csv('./data/20150803115609-HR_Retention_2013_training.csv')
-    df['is_train'] = np.random.uniform(0, 1, len(df)) <= .75
+    df['is_train'] = (df[myid] % 10) >= 3
+    # df['is_train'] = np.random.uniform(0, 1, len(df)) <= .75
     train, test = df[df['is_train']==True], df[df['is_train']==False]
-
 else:
     # To run with real data
     train = pd.read_csv('./data/20150803115609-HR_Retention_2013_training.csv')
@@ -67,43 +71,44 @@ for col in features:
     train[col] = scaler.transform(train[col])
     test[col] = scaler.transform(test[col])
 
-MyNNClassifier1 = Classifier(
+MyNNClassifier = Classifier(
                     layers=[
-                        Layer('Rectifier', units=100),
-                        Layer("Tanh", units=100),
-                        Layer("Sigmoid", units=100),
+                        Layer("Rectifier", units=100),
+                        Layer("Rectifier", units=100),
+                        Layer("Rectifier", units=100),
+                        Layer("Rectifier", units=100),
                         Layer('Softmax')],
-                    learning_rate=0.01,
+                    learning_rate=0.1,
                     learning_rule='momentum',
                     learning_momentum=0.9,
-                    batch_size=100,
+                    batch_size=20,
                     valid_size=0.01,
-                    n_stable=20,
-                    n_iter=200,
-                    verbose=True)
-
-MyNNClassifier2 = Classifier(
-                    layers=[
-                        Layer("Tanh", units=100),
-                        Layer("Tanh", units=100),
-                        Layer("Tanh", units=100),
-                        Layer("Sigmoid", units=100),
-                        Layer('Softmax')],
-                    learning_rate=0.01,
-                    learning_rule='momentum',
-                    learning_momentum=0.9,
-                    batch_size=100,
-                    valid_size=0.01,
-                    n_stable=20,
+                    n_stable=50,
                     n_iter=200,
                     verbose=True)
 
 # Define classifiers
 if sample:
     classifiers = [
-        # MyNNClassifier1,
-        # MyNNClassifier2,
-        XGBClassifier(n_estimators=128,subsample=0.6,max_depth=16,min_child_weight=3)
+        # KNeighborsClassifier(3),
+        # SVC(kernel="linear", C=0.025, probability=True),
+        # SVC(gamma=2, C=1, probability=True),
+        # RandomForestClassifier()
+        # RandomForestClassifier(max_depth=16, n_estimators=1024, max_features=None),
+        # AdaBoostClassifier(),
+        # GaussianNB(),
+        # LDA(),
+        # QDA(),
+        # MyNNClassifier,
+        # DecisionTreeClassifier(criterion='entropy', min_samples_split=2,
+        #     min_samples_leaf=1, max_depth=5, min_weight_fraction_leaf=0.0,
+        #     max_features=None, random_state=None, max_leaf_nodes=None, class_weight=None),
+        # AdaBoostClassifier(DecisionTreeClassifier(max_depth=16), algorithm="SAMME",n_estimators=200),
+        XGBClassifier(n_estimators=128,subsample=0.4,max_depth=16,min_child_weight=4),
+        XGBClassifier(n_estimators=512,subsample=0.8,max_depth=8,min_child_weight=5),
+        XGBClassifier(n_estimators=128,subsample=1,max_depth=16,min_child_weight=3),
+        XGBClassifier(n_estimators=128,subsample=1.2,max_depth=16,min_child_weight=3),
+        XGBClassifier(n_estimators=128,subsample=1.4,max_depth=16,min_child_weight=3)
     ]
 else:
     classifiers = [# Other methods are underperformed yet take very long training time for this data set
@@ -111,7 +116,7 @@ else:
         # MyNNClassifier2,
         # GradientBoostingClassifier(max_depth=16,n_estimators=1024),
         # RandomForestClassifier(max_depth=16,n_estimators=1024),
-        XGBClassifier(n_estimators=128,subsample=0.6,max_depth=16,min_child_weight=3)
+        XGBClassifier(n_estimators=128,subsample=1,max_depth=16,min_child_weight=3)
     ]
 
 # Train
@@ -132,15 +137,15 @@ for classifier in classifiers:
             if (classifier.__class__.__name__ == "XGBClassifier"):
                 print "Attempting GridSearchCV for XGB model"
                 gscv = GridSearchCV(classifier, {
-                    'max_depth': [2, 8, 16],
-                    'n_estimators': [64, 128, 256, 512, 1024],
+                    'max_depth': [2, 8, 16, 32],
+                    'n_estimators': [64, 128, 256, 512],
                     'min_child_weight': [3,5],
-                    'subsample': [0.6,0.8,1]},
+                    'subsample': [0.6,0.8,1,1.2,1.4]},
                     verbose=1, n_jobs=2, scoring='log_loss')
             if (classifier.__class__.__name__ == "RandomForestClassifier"):
                 print "Attempting GridSearchCV for RF model"
                 gscv = GridSearchCV(classifier, {
-                    'max_depth': [2, 8, 16],
+                    'max_depth': [2, 8, 16, 32],
                     'n_estimators': [32, 64, 128, 256, 512],
                     'bootstrap':[True,False],
                     'oob_score': [True,False]},
@@ -166,7 +171,8 @@ if sample:
         print classifier.__class__.__name__
         print 'Log Loss:'
         print log_loss(test[goal].values, classifier.predict_proba(np.array(test[features])))
-
+        print 'Accuracy Score:'
+        print accuracy_score(test[goal].values,classifier.predict(np.array(test[features])))
 else: # Export result
     count = 0
     for classifier in classifiers:
@@ -181,8 +187,8 @@ else: # Export result
             pass
         csvfile = 'result/' + classifier.__class__.__name__ + '-'+ str(count) + '-submit.csv'
         with open(csvfile, 'w') as output:
-            predictions = np.column_stack((test[myid],np.compress([False, True], classifier.predict_proba(np.array(test[features])), axis=1)))
-            predictions = [[int(i[0])] + i[1:] for i in predictions]
+            predictions = np.column_stack((test[myid], classifier.predict_proba(np.array(test[features])))).tolist()
+            predictions = [[int(i[0])] + i[2:3] for i in predictions]
             writer = csv.writer(output, lineterminator='\n')
             writer.writerow([myid,goal])
             writer.writerows(predictions)
