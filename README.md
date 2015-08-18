a7fbf90f53595b7b1ef45a6075a6e38d05250298
lenguyenthedat <thedat.lenguyen@gmail.com>
2015-08-18 17:31:11 +0800

    Clean up and feature engineering...

--- README.md
+++ README.md
@@ -16,3 +16,17 @@ XGBClassifier(n_estimators=512,subsample=2,max_depth=16,min_child_weight=7) 0.01
 XGBClassifier(n_estimators=365,subsample=1,max_depth=16,min_child_weight=9,learning_rate=0.1) 0.01987 with age grouping
 XGBClassifier(n_estimators=512,subsample=1,max_depth=8,min_child_weight=11,learning_rate=0.1) 0.0195721
 Blending / Stacking (see ens* folders)
+XGBClassifier(n_estimators=256,subsample=2,max_depth=16,min_child_weight=7) - removed ['GENDER','COUNTRY_OF_BIRTH','NATIONALITY','AGE'] - 0.0206067
+XGBClassifier(n_estimators=256,subsample=2,max_depth=16,min_child_weight=7) - fill w median, 0.0192577, rank grouping 1, 2 and both
+XGBClassifier(n_estimators=256,subsample=2,max_depth=16,min_child_weight=7) - fill w -1, 0.019219, rank grouping 1, 2 and both
+XGBClassifier(n_estimators=256,subsample=2,max_depth=16,min_child_weight=7) - fill w -1, 0.018724, no rank grouping at all
+XGBClassifier(n_estimators=256,subsample=2,max_depth=16,min_child_weight=7) - fill w -1, 0.0188001, no rank grouping at all, no scaler
+XGBClassifier(n_estimators=256,subsample=2,max_depth=16,min_child_weight=7) - fill w -1, and mean(if number), 0.0196049  no rank grouping at all
+XGBClassifier(n_estimators=256,subsample=2,max_depth=16,min_child_weight=7) - fill w 0,  0.0189772 no rank grouping at all
+XGBClassifier(n_estimators=256,subsample=2,max_depth=16,min_child_weight=7) - fill w -10,0.0190489 no rank grouping at all
+
+--- mod median, with rank groupings
+RandomForestClassifier(n_estimators=1024, max_features=23,oob_score=False, bootstrap=True, min_samples_leaf=1,min_samples_split=2, max_depth=32): 0.026679
+XGBClassifier(n_estimators=256,subsample=0.5,max_depth=10,min_child_weight=3,learning_rate=0.1) 0.0200443
+XGBClassifier(n_estimators=256,subsample=0.5,max_depth=10,min_child_weight=3,learning_rate=0.05) 0.0193743
+XGBClassifier(n_estimators=2048,subsample=0.5,max_depth=10,min_child_weight=3,learning_rate=0.03) 0.0204512
--- classify.py
+++ classify.py
@@ -6,6 +6,8 @@ import os
 import itertools
 import matplotlib.pyplot as plt
 import scipy as sp
+import warnings
+warnings.filterwarnings("ignore")
 
 from matplotlib.backends.backend_pdf import PdfPages
 from sklearn.metrics import make_scorer
@@ -24,7 +26,9 @@ from sklearn.base import TransformerMixin
 
 
 sample = True
-gridsearch = False
+gridsearch = True
+goal = 'RESIGNED'
+myid = 'PERID'
 
 def entropyloss(act, pred):
     epsilon = 1e-15
@@ -45,20 +49,15 @@ class DataFrameImputer(TransformerMixin):
         Columns of other types are imputed with mean of column.
         """
     def fit(self, X, y=None):
-        # self.fill = pd.Series([X[c].value_counts().index[0]
-        #     if X[c].dtype == np.dtype('O') else X[c].mean() for c in X],
-        #     index=X.columns)
-        self.fill = pd.Series([X[c].value_counts().index[0] # best for RF and ET
-            if X[c].dtype == np.dtype('O') else X[c].median() for c in X],
+        self.fill = pd.Series([X[c].value_counts().index[0] # mode
+            if X[c].dtype == np.dtype('O') else X[c].mean() for c in X], # mean
             index=X.columns)
-        # self.fill = pd.Series(['-1' # Treat N/A uniquely - best for XGB
-        #     if X[c].dtype == np.dtype('O') else -1 for c in X],
-        #     index=X.columns)
         return self
     def transform(self, X, y=None):
         return X.fillna(self.fill)
 
-features = ['GENDER','COUNTRY_OF_BIRTH','NATIONALITY','AGE',
+# LOAD DATA
+features = ['GENDER','COUNTRY_OF_BIRTH','NATIONALITY','AGE','AGE_GROUPING','MARITAL_STATUS',
             'YEARS_IN_GRADE','EMPLOYEE_GROUP','PARENT_SERVICE','SERVICE_SUB_AREA','SERVICE_TYPE','YEARS_OF_SERVICE',
             'VOC','UNIT','NO_OF_KIDS','MIN_CHILD_AGE','AVE_CHILD_AGE','HSP_ESTABLISHMENT','HSP_CERTIFICATE','HSP_CERT_RANK',
             'HSP_CERT_DESC','UPGRADED_LAST_3_YRS','UPGRADED_CERT_3_YRS','UPGRADED_CERT_DESC_3_YRS','MARRIED_WITHIN_2_YEARS',
@@ -66,16 +65,12 @@ features = ['GENDER','COUNTRY_OF_BIRTH','NATIONALITY','AGE',
             'PROMO_LAST_2_YRS','PROMO_LAST_1_YR','UNIT_CHG_LAST_3_YRS','UNIT_CHG_LAST_2_YRS','UNIT_CHG_LAST_1_YR','AWARDS_RECEIVED',
             'HOUSING_TYPE','HOUSING_GROUP','HOUSING_RANK','PREV_HOUSING_TYPE','MOVE_HOUSE_T_2','HOUSE_UPG_DGRD','IPPT_SCORE',
             'PES_SCORE','HOMETOWORKDIST','SVC_INJURY_TYPE','TOT_PERC_INC_LAST_1_YR','BAS_PERC_INC_LAST_1_YR']
-features_non_numeric = ['GENDER','COUNTRY_OF_BIRTH','NATIONALITY',
+features_non_numeric = ['GENDER','COUNTRY_OF_BIRTH','NATIONALITY','AGE_GROUPING','MARITAL_STATUS',
             'EMPLOYEE_GROUP','PARENT_SERVICE','SERVICE_SUB_AREA','SERVICE_TYPE',
             'VOC','UNIT','HSP_ESTABLISHMENT','HSP_CERTIFICATE',
             'HSP_CERT_DESC','UPGRADED_LAST_3_YRS','UPGRADED_CERT_3_YRS','UPGRADED_CERT_DESC_3_YRS','MARRIED_WITHIN_2_YEARS',
             'DIVORCE_WITHIN_2_YEARS','DIVORCE_REMARRIED_WITHIN_2_YEARS','UNIT_CHG_LAST_3_YRS','UNIT_CHG_LAST_2_YRS','UNIT_CHG_LAST_1_YR',
-            'HOUSING_TYPE','HOUSING_GROUP','PREV_HOUSING_TYPE','MOVE_HOUSE_T_2']
-
-goal = 'RESIGNED'
-myid = 'PERID'
-
+            'HOUSING_TYPE','HOUSING_GROUP','PREV_HOUSING_TYPE','MOVE_HOUSE_T_2','SVC_INJURY_TYPE']
 # Load data
 if sample:
     if gridsearch:
@@ -83,7 +78,7 @@ if sample:
         test = pd.read_csv('./data/20150803115608-HR_Retention_2013_to_be_predicted.csv')
     else:
         df = pd.read_csv('./data/20150803115609-HR_Retention_2013_training.csv')
-        df['is_train'] = (df[myid] % 17) >= 9
+        df['is_train'] = (df[myid] % 10) >= 7
         # df['is_train'] = np.random.uniform(0, 1, len(df)) <= .75
         train, test = df[df['is_train']==True], df[df['is_train']==False]
 else:
@@ -91,6 +86,47 @@ else:
     train = pd.read_csv('./data/20150803115609-HR_Retention_2013_training.csv')
     test = pd.read_csv('./data/20150803115608-HR_Retention_2013_to_be_predicted.csv')
 
+# FEATURE ENGINEERING
+# Rank grouping
+train['Rank_1'] = train['RANK_GROUPING'].apply(lambda x: x.split(' ')[0])
+train['Rank_2'] = train['RANK_GROUPING'].apply(lambda x: x.split(' ')[1] if len(x.split(' ')) > 1 else '')
+test['Rank_1'] = test['RANK_GROUPING'].apply(lambda x: x.split(' ')[0])
+test['Rank_2'] = test['RANK_GROUPING'].apply(lambda x: x.split(' ')[1] if len(x.split(' ')) > 1 else '')
+features = features + ['Rank_1', 'Rank_2', 'RANK_GROUPING']
+features_non_numeric = features_non_numeric + ['Rank_1', 'Rank_2', 'RANK_GROUPING']
+
+# These are yes / no columns which might contain NaN that doesn't have a significant propotion of yes or no
+for col in ['UNIT_CHG_LAST_3_YRS','UNIT_CHG_LAST_2_YRS','UNIT_CHG_LAST_1_YR','MOVE_HOUSE_T_2','UPGRADED_LAST_3_YRS']:
+    train[col] = train[col].fillna('UNKNOWN')
+    test[col] = test[col].fillna('UNKNOWN')
+
+# SVC Injury Type
+train['SVC_INJURY_TYPE'] = train['SVC_INJURY_TYPE'].fillna(-1)
+test['SVC_INJURY_TYPE'] = test['SVC_INJURY_TYPE'].fillna(-1)
+
+# HSP_ESTABLISHMENT
+train['HSP_ESTABLISHMENT'] = train['HSP_ESTABLISHMENT'].fillna('NONE')
+test['HSP_ESTABLISHMENT'] = test['HSP_ESTABLISHMENT'].fillna('NONE')
+
+# HSP_CERTIFICATE
+train['HSP_CERTIFICATE'] = train['HSP_CERTIFICATE'].fillna('NONE')
+test['HSP_CERTIFICATE'] = test['HSP_CERTIFICATE'].fillna('NONE')
+
+# UPGRADED_CERT_DESC_3_YRS - this has too many values
+
+# HOUSING_TYPE
+train['HOUSING_TYPE'] = train['HOUSING_TYPE'].fillna('NONE')
+test['HOUSING_TYPE'] = test['HOUSING_TYPE'].fillna('NONE')
+
+# HOUSING_GROUP
+train['HOUSING_GROUP'] = train['HOUSING_GROUP'].fillna('NONE')
+test['HOUSING_GROUP'] = test['HOUSING_GROUP'].fillna('NONE')
+
+# PREV_HOUSING_TYPE
+train['PREV_HOUSING_TYPE'] = train['PREV_HOUSING_TYPE'].fillna('UNKNOWN')
+test['PREV_HOUSING_TYPE'] = test['PREV_HOUSING_TYPE'].fillna('UNKNOWN')
+
+# Fill NA
 train = DataFrameImputer().fit_transform(train)
 test = DataFrameImputer().fit_transform(test)
 
@@ -108,61 +144,22 @@ for col in set(features)-set(features_non_numeric):
     train[col] = scaler.transform(train[col])
     test[col] = scaler.transform(test[col])
 
-MyNNClassifier = Classifier(
-            layers=[
-                Layer('Rectifier', units=200),
-                Layer('Rectifier', units=256),
-                Layer('Softmax')],
-            learning_rate=0.01,
-            learning_rule='momentum',
-            learning_momentum=0.9,
-            batch_size=2000,
-            valid_size=0.01,
-            n_stable=100,
-            n_iter=100,
-            verbose=False)
-
-# Define classifiers
+# CLASSIFIERS DEFINED
 if sample:
     classifiers = [
-        # KNeighborsClassifier(3),
-        # SVC(kernel="linear", C=0.025, probability=True),
-        # SVC(gamma=2, C=1, probability=True),
-        # RandomForestClassifier(),
-        # RandomForestClassifier(max_depth=16, n_estimators=1024, max_features=None),
-        # AdaBoostClassifier(),
-        # GaussianNB(),
-        # LDA(),
-        # QDA(),
-        # MyNNClassifier
-        # DecisionTreeClassifier(criterion='entropy', min_samples_split=2,
-        #     min_samples_leaf=1, max_depth=5, min_weight_fraction_leaf=0.0,
-        #     max_features=None, random_state=None, max_leaf_nodes=None, class_weight=None),
-        # AdaBoostClassifier(DecisionTreeClassifier(max_depth=16), algorithm="SAMME",n_estimators=200),
         # ExtraTreesClassifier(n_estimators=1024, max_features=None,
         #                        oob_score=False, bootstrap=True, min_samples_leaf=1,
         #                        min_samples_split=2, max_depth=19),
         # RandomForestClassifier(n_estimators=1024, max_features=23,
         #                        oob_score=False, bootstrap=True, min_samples_leaf=1,
         #                        min_samples_split=2, max_depth=32),
-        # RandomForestClassifier(n_estimators=512, max_features=16,
-        #                        oob_score=False, bootstrap=True, min_samples_leaf=2,
-        #                        min_samples_split=4, max_depth=8),
-        # XGBClassifier(n_estimators=512,subsample=1,max_depth=10,min_child_weight=8,learning_rate=0.05),
         # XGBClassifier(n_estimators=256,subsample=2,max_depth=16,min_child_weight=7)
-        XGBClassifier(),
-        RandomForestClassifier(),
-        ExtraTreesClassifier()
+        XGBClassifier()
+        # RandomForestClassifier(),
+        # ExtraTreesClassifier()
     ]
 else:
     classifiers = [# Other methods are underperformed yet take very long training time for this data set
-        # MyNNClassifier1,
-        # MyNNClassifier2,
-        # GradientBoostingClassifier(max_depth=16,n_estimators=1024),
-        # RandomForestClassifier(max_depth=16,n_estimators=1024),
-        # XGBClassifier(n_estimators=128,subsample=1,max_depth=16,min_child_weight=3),
-        # XGBClassifier(n_estimators=256,subsample=2,max_depth=16,min_child_weight=7),
-        # RandomForestClassifier(max_depth=16, n_estimators=256, max_features=8),
         ExtraTreesClassifier(n_estimators=1024, max_features=None,
                                oob_score=False, bootstrap=True, min_samples_leaf=1,
                                min_samples_split=2, max_depth=19),
@@ -172,33 +169,25 @@ else:
         XGBClassifier(n_estimators=256,subsample=2,max_depth=16,min_child_weight=7)
     ]
 
-# Train
+# TRAINING / GRIDSEARCH
 for classifier in classifiers:
     print classifier.__class__.__name__
     start = time.time()
     if (gridsearch & sample): # only do gridsearch if we run with sampled data.
-        if (classifier.__class__.__name__ == "GradientBoostingClassifier"):
-            print "Attempting GridSearchCV for GB model"
-            gscv = GridSearchCV(classifier, {
-                'max_depth': [2, 8, 16],
-                'n_estimators': [32, 64, 128, 256, 512],
-                'learning_rate': [0.1, 0.05, 0.01],
-                'subsample': [0.6,0.8,1]},
-                verbose=1, n_jobs=1, cv=3, scoring=el_scorer)
         if (classifier.__class__.__name__ == "XGBClassifier"):
             print "Attempting GridSearchCV for XGB model"
             gscv = GridSearchCV(classifier, {
-                'max_depth': [8, 10, 13, 16, 20],
+                'max_depth': [12,16,20,24,28],
                 'n_estimators': [256, 300, 365, 512],
-                'min_child_weight': [1,2,3,4,5,6,7,8,9],
-                'subsample': [0.5,1,1.5,2,2.5],
+                'min_child_weight': [1,3,5,7,9],
+                'subsample': [0.5,1,1.5,2],
                 'learning_rate': [0.1, 0.05, 0.01]},
                 verbose=1, n_jobs=1, cv=3, scoring=el_scorer)
         if (classifier.__class__.__name__ == "RandomForestClassifier"):
             print "Attempting GridSearchCV for RF model"
             gscv = GridSearchCV(classifier, {
-                'max_depth': [12,14,16,18,20,22,24,26,28,30],
-                'max_features' : [2, 8, 16, 20, 24, 28, 32],
+                'max_depth': [12,16,20,24,28],
+                'max_features' : [2, 8, 16, 24, 32],
                 'min_samples_split': [1,2,4,6,8],
                 'min_samples_leaf': [1,2,3,4],
                 'n_estimators': [256, 512, 1024],
@@ -208,20 +197,14 @@ for classifier in classifiers:
         if (classifier.__class__.__name__ == "ExtraTreesClassifier"):
             print "Attempting GridSearchCV for ExtraTrees model"
             gscv = GridSearchCV(classifier, {
-                'max_depth': [12,14,16,18,20,22,24,26,28,30],
-                'max_features' : [2, 8, 16, 20, 24, 28, 32],
+                'max_depth': [12,16,20,24,28],
+                'max_features' : [2, 8, 16, 24, 32],
                 'min_samples_split': [1,2,4,6,8],
                 'min_samples_leaf': [1,2,3,4],
                 'n_estimators': [256, 512, 1024],
                 'bootstrap':[True],
                 'oob_score': [True,False]},
                 verbose=1, n_jobs=1, cv=3,scoring=el_scorer)
-        if (classifier.__class__.__name__ == "Classifier"): # NN Classifier
-            print "Attempting GridSearchCV for Neural Network model"
-            gscv = GridSearchCV(classifier, {
-                'hidden0__units': [4, 16, 32, 64, 128, 256],
-                'hidden0__type': ["Rectifier", "Sigmoid", "Tanh"]},
-                verbose=1, n_jobs=1)
         classifier = gscv.fit(np.array(train[list(features)]), train[goal])
         print(classifier.best_score_)
         print(classifier.best_params_)
@@ -232,7 +215,8 @@ for classifier in classifiers:
         print classifier.feature_importances_
     except:
         pass
-# Evaluation and export result
+
+# EVAL OR EXPORT
 if sample:
     if not gridsearch:
         for classifier in classifiers:
@@ -271,6 +255,6 @@ else: # Export result
                 plt.xlabel('Relative Importance')
                 plt.title('Variable Importance')
                 pdf.savefig()
-                # plt.show()
+                plt.show()
         except:
             pass
